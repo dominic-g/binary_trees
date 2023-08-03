a78eb038a50bba5e235df4c90d8b7f822dc50962
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-03 07:21:38 -0400

    0-binary_tree_node.c, binary_tree header & README Files Betty styling

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,20 +1,22 @@
 #include "binary_trees.h"
 /**
- * binary_tree_node - creates new node in tree
- * @parent: pointer to parent
- * @value: value of new node
- * Return: pointer to new node
+ * binary_tree_node - Creates a new binary tree node.
+ * @parent: Pointer to the parent node.
+ * @value: Value to be stored in the new node.
+ *
+ * Return: Pointer to the new node, or NULL if memory allocation fails.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+binary_tree_t *new_node;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!(new))
-		return (NULL);
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-	return (new);
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+
+return (new_node);
 }
