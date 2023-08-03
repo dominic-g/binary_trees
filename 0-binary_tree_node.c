e502303f0cc31d7f9663444d1047d5dc9e68858e
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-03 07:24:47 -0400

    0-binary_tree_node.c, binary_tree header & README Files Betty styling

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *new_node;
 new_node = malloc(sizeof(binary_tree_t));
 if (new_node == NULL)
 return (NULL);
-new_node->value = value;
+new_node->n = value;
 new_node->parent = parent;
 new_node->left = NULL;
 new_node->right = NULL;
